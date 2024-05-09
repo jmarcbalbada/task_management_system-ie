53d8c543fcec8b086745def3081bf63306125fa1
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-05-09 13:12:10 +0800

    [Marc] - configuring backend

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,11 @@ app.use(express.json());
 
 //google
 const db = mysql.createConnection({
-  socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
+  // socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
   host: "35.236.169.7",
   user: "root",
   password: "",
-  database: "task_management",
+  database: "taskmanagement",
 });
 
 // Start the database connection
