6365a55b34480e6b37887107333648794ac10714
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-05-09 13:48:09 +0800

    [Marc] - configuring config.js

--- frontend/src/data/configure.js
+++ frontend/src/data/configure.js
@@ -6,8 +6,8 @@ if (!isLocal) {
   // Assuming your backend is not running on the same machine as your frontend
   // Adjust the IP address accordingly if your backend is running on a different machine
   //   apiUrl = 'http://172.16.103.217:8000/';
-  // apiUrl = "http://localhost:3000/";
-  apiUrl = "https://task-management-system-ie-back.vercel.app/";
+  apiUrl = "http://localhost:3000/";
+  // apiUrl = "https://task-management-system-ie-back.vercel.app/";
 }
 
 const config = {
