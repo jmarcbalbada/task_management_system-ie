960fa24f31e01a3791e6d37f44d47b71f250acac
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-05-09 13:33:01 +0800

    [Marc] - configuring backend

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,25 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const { Connection } = require("@google-cloud/sql");
+// const { Connection } = require("@google-cloud/sql");
 
 // Create a new connection to Google Cloud SQL
-const db = new Connection({
-  connectionName: "peppy-appliance-418100:asia-east1:taskmanagement",
-  credentials: {
-    client_email: "370147750627-compute@developer.gserviceaccount.com",
-    private_key: "897f01284bf451b7c9b339d2bfdd866af14cdd92",
-  },
-});
+// const db = new Connection({
+//   connectionName: "peppy-appliance-418100:asia-east1:taskmanagement",
+//   credentials: {
+//     client_email: "370147750627-compute@developer.gserviceaccount.com",
+//     private_key: "897f01284bf451b7c9b339d2bfdd866af14cdd92",
+//   },
+// });
 
-// Connect to the database
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to Google Cloud SQL:", err);
-    return;
-  }
-  console.log("Connected to Google Cloud SQL");
-});
+// // Connect to the database
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to Google Cloud SQL:", err);
+//     return;
+//   }
+//   console.log("Connected to Google Cloud SQL");
+// });
 
 // local
 // const db = mysql.createConnection({
@@ -42,22 +42,22 @@ db.connect((err) => {
 // });
 
 // //google
-// const db = mysql.createConnection({
-//   // socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
-//   host: "35.236.169.7",
-//   user: "root",
-//   password: "",
-//   database: "task_management",
-// });
+const db = mysql.createConnection({
+  // socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
+  host: "35.236.169.7",
+  user: "root",
+  password: "",
+  database: "task_management",
+});
 
 // // Start the database connection
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to the database:", err);
-//     return;
-//   }
-//   console.log("Connected to MySQL database");
-// });
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
 
 // Export the db connection so it can be imported in other modules
 module.exports.db = db;
