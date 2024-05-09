447d3e3f881960695bf13d76ea9930ca17042d9a
John Marc Balbada <91250096+jmarcbalbada@users.noreply.github.com>
2024-05-09 13:24:11 +0800

    [Marc] - configuring backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,25 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+const { Connection } = require("@google-cloud/sql");
+
+// Create a new connection to Google Cloud SQL
+const db = new Connection({
+  connectionName: "peppy-appliance-418100:asia-east1:taskmanagement",
+  credentials: {
+    client_email: "370147750627-compute@developer.gserviceaccount.com",
+    private_key: "897f01284bf451b7c9b339d2bfdd866af14cdd92",
+  },
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to Google Cloud SQL:", err);
+    return;
+  }
+  console.log("Connected to Google Cloud SQL");
+});
 
 // local
 // const db = mysql.createConnection({
@@ -22,23 +41,23 @@ app.use(express.json());
 //   database: "bp64edmhqzhq1vg1azkm",
 // });
 
-//google
-const db = mysql.createConnection({
-  // socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
-  host: "35.236.169.7",
-  user: "root",
-  password: "",
-  database: "task_management",
-});
+// //google
+// const db = mysql.createConnection({
+//   // socketPath: "/cloudsql/peppy-appliance-418100:asia-east1:taskmanagement",
+//   host: "35.236.169.7",
+//   user: "root",
+//   password: "",
+//   database: "task_management",
+// });
 
-// Start the database connection
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
+// // Start the database connection
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to the database:", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL database");
+// });
 
 // Export the db connection so it can be imported in other modules
 module.exports.db = db;
